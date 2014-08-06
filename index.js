e0bda6a8b7572e2315e12ffc4709c09447a50676
Stephen Whitmore <stephen.whitmore@gmail.com>
2014-08-05 20:10:51 -0700

    Initial commit. Bare bones tests and impl.

---
+++ index.js
@@ -0,0 +1,68 @@
+function World() {
+  this.entityDefs = {};
+}
+
+World.prototype.runAs = function(mode, opts) {
+};
+
+World.prototype.defineEntity = function(name, opts) {
+  this.entityDefs[name] = opts;
+};
+
+// Create a new entity that is local to THIS machine.
+World.prototype.createEntity = function(name) {
+  var def = this.entityDefs[name];
+  if (def === null || def === undefined) {
+    return null;
+  }
+
+  var entity = new Entity();
+
+  // Local entities are always Authority.
+  entity.role = Role.Authority;
+
+  // We rely on whoever writes the entity def to set a remoteRole, if they
+  // don't want the default of None.
+  entity.remoteRole = Role.None;
+
+  // Populate vars.
+  for (var varName in def.vars) {
+    entity[varName] = def.vars[varName];
+  }
+
+  // Populate funcs.
+  for (var funcName in def.funcs) {
+    (function(name, f) {
+      entity[name] = function() {
+        f.call(entity);
+      };
+    })(funcName, def.funcs[funcName]);
+  }
+
+  return entity;
+};
+
+
+function Entity() {
+}
+
+
+var NetworkMode = {
+  Local: 0,
+  Server: 1,
+  Client: 2,
+};
+
+var Role = {
+  Authority: 10,
+  AutonomousProxy: 8,
+  SimulatedProxy: 6,
+  DumbProxy: 4,
+  None: 2,
+};
+
+
+module.exports.NetworkMode = NetworkMode;
+module.exports.Role = Role;
+module.exports.World = World;
+
---
+++ tests/local.js
@@ -0,0 +1,95 @@
+var repl = require('../index');
+var test = require('tape');
+
+function localStockWorld() {
+  var world = new repl.World();
+
+  // NEEDS:
+  //  A way of creating client endpoints
+  //
+  world.runAs(repl.NetworkMode.Standalone, {
+  // Only needed in Client or Server modes
+   // host: 'foo.example.org',
+   // port: 8000,
+  });
+
+  world.defineEntity("point-mass", {
+    vars: {
+      x: 0,
+      y: 0,
+      mass: 1,
+    },
+
+    funcs: {
+      teleportFixed: function() {
+        this.x = 50;
+        this.y = 60;
+      },
+
+      teleportTo: function(toX, toY) {
+        this.x = toX;
+        this.y = toY;
+      },
+    },
+
+    replication: function(vars) {
+      if (this.role === repl.Role.Authority) {
+        vars.push('x', 'y', 'mass');
+      }
+    },
+  });
+
+  return world;
+}
+
+test('unknown entity def', function(t) {
+  var world = localStockWorld();
+
+  var car = world.createEntity("car");
+  t.equal(car, null);
+
+  t.end();
+});
+
+test('create local entity', function(t) {
+  var world = localStockWorld();
+
+  var pmass = world.createEntity("point-mass");
+  t.equal(pmass.mass, 1);
+  t.equal(pmass.x, 0);
+
+  pmass.x = 17;
+  t.equal(pmass.mass, 1);
+  t.equal(pmass.x, 17);
+
+  t.end();
+
+  // Q: how do I set up a server and hook it up to the world?
+  // Q: how do ReplicationChannels get set up?
+});
+
+test('local entity functions', function(t) {
+  var world = localStockWorld();
+
+  var pmass = world.createEntity("point-mass");
+  t.equal(pmass.x, 0);
+  t.equal(pmass.y, 0);
+
+  pmass.teleportFixed();
+
+  t.equal(pmass.x, 50);
+  t.equal(pmass.y, 60);
+
+  t.end();
+});
+
+test('local role', function(t) {
+  var world = localStockWorld();
+
+  var pmass = world.createEntity("point-mass");
+  t.equal(pmass.role, repl.Role.Authority);
+  t.equal(pmass.remoteRole, repl.Role.None);
+
+  t.end();
+});
+
